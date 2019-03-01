2a625ade38e8d54ee50b4805ed9f6559b757eef0
Jeechu Deka <jeechu@getsimpl.com>
2019-03-01 20:11:11 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 from version import VERSION
 
 requirements = []
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='cloudlift',
     version=VERSION,
     packages=find_packages(),
     install_requires=requirements,
+    description="Cloudlift makes it easier to launch dockerized services in AWS ECS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GetSimpl/cloudlift",
     zip_safe=True,
     entry_points='''
         [console_scripts]
