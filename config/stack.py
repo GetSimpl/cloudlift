6a413ad0f555f7252ad7d1a28aca75cea08c6cd8
Jeechu Deka <jeechu@getsimpl.com>
2019-03-26 12:09:09 +0530

    remove Simpl specific config

--- config/stack.py
+++ config/stack.py
@@ -1,15 +1,6 @@
-# TODO: Simpl
 def get_cluster_name(environment):
-    if environment == 'unicorn':
-        environment_name = environment + "-cluster"
-    else:
-        environment_name = "cluster-" + environment
-    return environment_name
+    return "cluster-" + environment
 
 
 def get_service_stack_name(environment, name):
-    if environment == 'unicorn':
-        stack_name = '-'.join([environment, name])
-    else:
-        stack_name = '-'.join([name, environment])
-    return stack_name
+    return '-'.join([name, environment])
