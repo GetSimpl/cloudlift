a4812b7134ae52b6d47a3bedd1493d6519b86bb9
praveenraghav01 <praveensinghraghav96@gmail.com>
2023-02-02 13:38:02 +0530

    added banner for username and account id

--- cloudlift/config/banner.py
+++ cloudlift/config/banner.py
@@ -1,3 +1,5 @@
+from cloudlift.config.account import get_user_id
+
 def highlight_production():
     print('''\033[91m
 ********************************
@@ -5,3 +7,10 @@ def highlight_production():
 **    You're on PRODUCTION    **
 ********************************\033[0m
 ''')
+
+def highlight_user_account_details():
+    username, account = get_user_id()
+    print('''\033[93m 
+******* Using Aws account {account} and Username {username} *******\033[0m
+'''.format(**locals()))
+    
